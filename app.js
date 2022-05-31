e42a368201b24fc1cfbb8f2788ebd6c7eaac6519
Matthew Gebhart <Matthewgebhart@gmail.com>
2022-05-31 02:05:20 -0700

    added some loop functions - some work, some dont...

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-function otterLoveTest(){
-    let otterAnswer = prompt("Do you love otters?");
-    if (otterAnswer == "yes")
-    document.write("Welcome fellow otter lover!");    
-    if (otterAnswer != "yes")
-    document.write("That is too bad, look at how cute they are and maybe reconsider?")
+// function otterLovePrompt(){
+//     let otterAnswer = prompt("Do you love otters?");
+//     if (otterAnswer == "yes")
+//     document.write("Welcome fellow otter lover!");    
+//     if (otterAnswer != "yes")
+//     document.write("That is too bad, look at how cute they are and maybe reconsider?"); 
+// }
+
+// otterLovePrompt();
+
+function otterTypeNumber(){
+    let otterTypeAnswer = prompt("how many distinct species of otters exist?");
+    if (otterTypeAnswer == "13") 
+    document.write("<h3>CORRECT! There are 13 types of otters around the world</h3>"); 
+    if (otterTypeAnswer > 13)
+    document.write("<h3>Incorrect, there are less otter types than that</h3>");
+    else if (otterTypeAnswer <= 12)
+    document.write("<h3>Incorrect, there are more otter types than that</h3>");
 }
 
-otterLoveTest();
+// otterTypeNumber();
+
+
+// function otterPics(){
+// let otterPicsAnswer = prompt("How many otter pictures do you want to see?");
+// if (otterPicsAnswer == "1")
+// // document.write(Image src="https://www.masslive.com/resizer/LPyU4yw5XvJAGj1Q6XdT8VdKs2A=/1280x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/5HPX3OOISVEYVHAITKDJOOAVGU.jpg");
+
+// }
